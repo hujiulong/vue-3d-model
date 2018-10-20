e609c9145a95c425a4de1ba6e2d866881b7ecb73
Jiulong Hu <me@hujiulong.com>
2018-10-20 16:07:25 +0800

    feat(gltf): set the default value of gammaOutput to true

--- src/model-gltf.vue
+++ src/model-gltf.vue
@@ -22,6 +22,10 @@ export default {
                     }
                 ]
             }
+        },
+        gammaOutput: {
+            type: Boolean,
+            default: true
         }
     },
     data() {
