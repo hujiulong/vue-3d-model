d68c65493a4a2007afcf9dae3f31e7c44eebd483
hujiulong <me@hujiulong.com>
2020-03-04 12:08:56 +0800

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-useless-escape': 'off',
     'max-len': 'off',
